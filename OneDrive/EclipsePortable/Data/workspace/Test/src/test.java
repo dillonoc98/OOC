b8973f6a900da06db41d241d26e1f6b9b7e1157a
dillo <dillo@192.168.0.108>
2018-10-24 01:34:29 +0100

    This is seemingly working?

---
+++ OneDrive/EclipsePortable/Data/workspace/Test/src/test.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Howya Pig!");
+		int i =9;
+		int x = 3;
+		int y = 6;
+		i=x+y;
+		System.out.println(i);
+	}
+
+}
